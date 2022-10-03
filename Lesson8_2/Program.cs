659256a2f2c32f251f5a9cc06768ef3a2c637f8a
spaceshipwalls@gmail.com <spaceshipwalls@gmail.com>
2022-10-03 19:36:56 +0300

    Add project files.

---
+++ Lesson8_2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lesson8_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "hello yarin";
+            //אורך של המחרוזת 
+            Console.WriteLine(name.Length);
+
+        }
+    }
+}
